685a503e38743bf7b08c24077f0254d4b02496e7
MaxwellsDaemon-ca <thejollycanadian@yahoo.com>
2024-12-01 13:14:19 -0500

    Redo of Code, hopefully it will work this time

---
+++ src/main/java/com/activity2/usermanagement/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.activity2.usermanagement.repository;
+
+import com.activity2.usermanagement.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
